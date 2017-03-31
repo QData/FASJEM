febc24a2ffc8b5684546c2d208b196b9f07ff74b
blw921 <bw4mw@virginia.edu>
2017-03-31 11:43:08 -0400

    add the manual

--- fasjem-cran/fasjem/R/fasjem.R
+++ fasjem-cran/fasjem/R/fasjem.R
@@ -1,4 +1,5 @@
 norm_vec <- function(x) sqrt(sum(x^2))
+norm_infty <- function(x) max(abs(x))
 
 f1 <- function(x, ga){
   result = sign(x) * pmax(abs(x)-ga, 0)
@@ -20,6 +21,16 @@ g2 <- function(x, a, lambda){
   return(result)
 }
 
+f2_infty <- function(x, ga){
+  result = x * array(pmax(1 - ga / apply(x, c(1,2), norm_infty), 0), dim(x))
+  return(result)
+}
+
+g2_infty <- function(x, a, lambda){
+  result = array(pmax(lambda / apply(x - a, c(1,2), norm_infty), 1), dim(x)) * (x - a) + a
+  return(result)
+}
+
 fasjem_g <- function(a, lambda, epsilon, gamma, rho, iterMax){
   x  = f1(a, (4*lambda*gamma))
   x1 = x
@@ -31,6 +42,28 @@ fasjem_g <- function(a, lambda, epsilon, gamma, rho, iterMax){
     p2 = g1(x2, a, (4*lambda*gamma*epsilon))
     p3 = f2(x3, lambda)
     p4 = g2(x4, a, (epsilon*lambda))
+    p  = (p1 + p2 + p3 + p4) / 4
+    x1 = x1 + (p * 2 - p1 - x1) * rho
+    x2 = x2 + (p * 2 - p2 - x2) * rho
+    x3 = x3 + (p * 2 - p3 - x3) * rho
+    x4 = x4 + (p * 2 - p4 - x4) * rho
+    x  = x  + (p - x) * rho
+  }
+  x = f1(x, (4*lambda*gamma))
+  return(x)
+}
+
+fasjem_i <- function(a, lambda, epsilon, gamma, rho, iterMax){
+  x  = f1(a, (4*lambda*gamma))
+  x1 = x
+  x2 = x
+  x3 = x
+  x4 = x
+  for (i in 1:iterMax){
+    p1 = f1(x1, (4*lambda*gamma))
+    p2 = g1(x2, a, (4*lambda*gamma*epsilon))
+    p3 = f2_infty(x3, lambda)
+    p4 = g2_infty(x4, a, (epsilon*lambda))
     p  = (p1 + p2 + p3 + p4) / 4
     x1 = x1 + (p * 2 - p1 - x1) * rho
     x2 = x2 + (p * 2 - p2 - x2) * rho
@@ -60,7 +93,7 @@ backwardMap <-function(covMatrix){
   return(solve(EEGM(covMatrix, bestniu)))
 }
 
-fasjem <- function(X, lambda, epsilon, gamma, rho, iterMax){
+fasjem <- function(X, method = "fasjem-g", lambda, epsilon, gamma, rho, iterMax){
   tmp = array(0, dim = c(dim(X[[1]])[1], dim(X[[1]])[2], length(X)))
   for (i in 1:length(X)){
     tmp[,,i] = X[[i]]
@@ -70,7 +103,12 @@ fasjem <- function(X, lambda, epsilon, gamma, rho, iterMax){
   }
 
   tmp = array(apply(tmp, 3, backwardMap), dim = c(ncol(X[[i]]), ncol(X[[i]]), length(X)))
+  if(method == "fasjem-g"){
   tmp = fasjem_g(tmp, lambda, epsilon, gamma, rho, iterMax)
+  }
+  if(method == "fasjem-i"){
+    tmp = fasjem_i(tmp, lambda, epsilon, gamma, rho, iterMax)
+  }
   result = list()
   for (i in 1:dim(tmp)[3]){
     result[[i]] = tmp[, , i]
